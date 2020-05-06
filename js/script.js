627251bfabc0dc208506fd65d5151ac6ede71166
Debatosh Pal Majumder <36793454+debatoshpm@users.noreply.github.com>
2020-05-06 07:09:22 +0530

    new

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ function first()
     $(".details").removeClass("noshow");
     $(".details").addClass("show");
 }
+
+function loadDoc(i) {
+    var xhttp;
+    if (window.XMLHttpRequest) {
+        xhttp = new XMLHttpRequest();
+    } else {
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("demo").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "../xml/semester.xml", true);
+    xhttp.send();
+}
